26438811443217414c56e2570561f74ac25acd5b
hoangpq <hoang.phan@xolv.org>
2023-01-31 10:23:10 +0700

    Update wasm build

--- wasm.ts
+++ wasm.ts
@@ -1,11 +1,12 @@
+#!/usr/bin/env -S deno run --allow-read --allow-write --v8-flags=--experimental-wasm-return_call,--no-liftoff,--wasm-lazy-compilation,--wasm-lazy-validation
+import fs from "node:fs"
 import WasiContext from "https://deno.land/std/wasi/snapshot_preview1.ts";
 
 const context = new WasiContext({});
-
 const encoder = new TextEncoder("utf-8");
 
 const instance = (
-  await WebAssembly.instantiate(await Deno.readFile("hello.wasm"), {
+  await WebAssembly.instantiate(fs.readFileSync("hello.wasm"), {
     wasi_snapshot_preview1: context.exports,
   })
 ).instance;
@@ -22,3 +23,7 @@ mem.set(buf, ptr);
 
 // will print Hello, <name> in the console
 instance.exports.hello(ptr);
+const buf2 = encoder.encode('Hoang Phan!');
+const ptr2 = instance.exports.mallocBytes(buf2.length);
+mem.set(buf2, ptr2);
+instance.exports.hello(ptr2);
